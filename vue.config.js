306dc1ec40dd137e490ed45e556546c5293b9f60
huang_chuojun <huang_chuojun@126.com>
2020-12-02 14:52:38 +0800

    整体布局样式优化

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            // 给 less 传递选项
-            less: {
-                //添加时报错,不能换行，所以一下配置
-                javascriptEnabled: true
-            }
-        }
-    }
-}
+  css: {
+    loaderOptions: {
+      // 给 less 传递选项
+      less: {
+        // 添加时报错,不能换行，所以一下配置
+        javascriptEnabled: true,
+      },
+    },
+  },
+};
